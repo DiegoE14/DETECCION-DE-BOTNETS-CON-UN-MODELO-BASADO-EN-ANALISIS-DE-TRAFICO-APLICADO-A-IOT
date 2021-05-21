b7374c3121ec5df9f0c7d0307a0421ce78ee1676
Oscar <rodriguezinsuastyoscar@gmail.com>
2021-05-20 19:29:19 -0500

    Se agrego al modelo de regresión logistica la grafica de curvas de ROC y matriz de confusión

--- modelo_regresion_logistica.py
+++ modelo_regresion_logistica.py
@@ -1,6 +1,8 @@
 from preparacion_data import Xtrain,Xtest,Ytrain,Ytest
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report,roc_curve,auc,plot_confusion_matrix
 from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt2
+from matplotlib import pyplot as plt3
 from sklearn.linear_model import LogisticRegression
 
 #Entrenamiento de Regresion Logistica
@@ -40,4 +42,27 @@ plt.plot(Grafica,c='r',label="Predicción del ataque")
 plt.legend(loc='upper left')
 plt.show()
 
+#Grafica CURVA ROC y AUC
+logns_probs = [0 for _ in range(len(Ytest))]
+loglr_probs = regresion_logistica.predict(Xtest)
 
+logns_fpr,logns_tpr, umbral = roc_curve(Ytest,logns_probs)
+auc_logns = auc (logns_fpr,logns_tpr)
+loglr_fpr,loglr_tpr, umbral = roc_curve(Ytest,loglr_probs)
+auc_log = auc (loglr_fpr,loglr_tpr)
+
+plt2.figure(figsize=(5,5),dpi=100)
+plt2.plot(logns_fpr,logns_tpr, linestyle ='--',label = 'Sin entrenar (auc = %0.2f)' %
+auc_logns)
+plt2.plot(loglr_fpr,loglr_tpr, marker ='.',label = 'Regreción Logistica (auc = %0.2f)' %
+auc_log)
+plt2.xlabel('Tasa de Falsos Positivos')
+plt2.ylabel('Tasa de Verdaderos Positivos')
+plt2.title('Curva ROC')
+plt2.legend()
+plt2.show()
+
+#Matrix de confusión
+plot_confusion_matrix(regresion_logistica, Xtest, Ytest)
+plt3.title('Matriz de Confusión')
+plt3.show()
